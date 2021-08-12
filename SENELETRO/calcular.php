4986d5292e9164f5fca076c6dfb3ca9d94b587ac
KELLY MARQUES BALAZSHAZI <21192911@CFP127ALUNO.LOCAL>
2021-08-12 09:38:58 -0300

    finish

--- SENELETRO/calcular.php
+++ SENELETRO/calcular.php
@@ -1,16 +1,16 @@
 <?php
 
 $nome = $_POST["nome"];
-$endereço = $_POST["endereço"];
-$Consumo = $_POST["Consumo"];
+$endereco = $_POST["endereco"];
+$Consumos = $_POST["consumo"];
 $total;
 
-if($Consumo >  120 ){
-    $total = $Consumo / 0.42;
+if($Consumos >  120 ){
+    $total = $Consumos / 0.42;
     $classeRed;
     $texto = "Obrigado(a) por usar nossa companhia";
 }else{
-    $total = $Consumo / 0.32;
+    $total = $Consumos / 0.32;
     $classeGreen;
     $texto = "Obrigado(a) por economizar!!!";
 }
@@ -24,12 +24,12 @@ if($Consumo >  120 ){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
-        <title>Document</title>
+        <title>Calcular</title>
     </head>
     <body class= "<?= $classeRed ?>" class="<?= $classGreen ?>">
         <h1> Conta de luz de <?= $nome ?></h1>
         <h3><?= $endereço ?></h3>
-        <h3 class="consumos">Consumo: <?= $Consumo ?></h3>
+        <h3 class="consumos">Consumo: <?= $Consumos ?></h3>
         <h3> Valor a ser pago: R$ <?= number_format($total,2) ?></h3> 
         <h1 class= "consumos"><?= $texto ?></h1>
     </body>
