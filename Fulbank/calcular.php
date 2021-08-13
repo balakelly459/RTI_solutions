d2805998b9d1687f2ce5dc37b638d12aeea66bba
KELLY MARQUES BALAZSHAZI <21192911@CFP127ALUNO.LOCAL>
2021-08-13 10:31:46 -0300

    finish

--- Fulbank/calcular.php
+++ Fulbank/calcular.php
@@ -1,16 +1,38 @@
+
 <?php
 
 if(isset($_REQUEST["nome"]) && isset($_REQUEST["salario"]) && isset($_REQUEST["cargos"])){
     $nome = $_REQUEST["nome"];
     $salario = $_REQUEST["salario"];
     $cargos = $_REQUEST["cargos"];
+
+    // $salario = 0;
     
     if($salario > 5000){
-        $total = $salario * 110/100;
-        echo "<h1>O $nome passará a receber R$$total, no cargo de $cargos</h1>";
+        $total = $salario * 1.1;
+       
     }else{
-        $total = $salario * 120/100;
-        echo "<h1>O $nome passará a receber R$$total, no cargo de $cargos </h1>";
+        $total = $salario * 1.2;
+       
     }
 
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular salário</title>
+    <link href="./style.css" rel="stylesheet"/>
+</head>
+<body>
+    <main>
+        <p><?=$nome?> passará a receber R$<?=$total?>, no cargo de <?=$cargos?></p>
+    </main>
+    
+</body>
+</html>
